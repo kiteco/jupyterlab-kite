8a4187aa3e7efba4816803fc4151185a2aa44a55
Ryan Berg <ryan@kite.com>
2020-05-06 10:02:32 -0700

    don't attempt to use default language servers

--- py_src/jupyter_kite/specs/__init__.py
+++ py_src/jupyter_kite/specs/__init__.py
@@ -1,24 +1,6 @@
 """ default specs
 """
 # flake8: noqa: F401
-from .bash_language_server import BashLanguageServer
-from .dockerfile_language_server_nodejs import DockerfileLanguageServerNodeJS
-from .javascript_typescript_langserver import JavascriptTypescriptLanguageServer
 from .kitels import KiteLanguageServer
-from .r_languageserver import RLanguageServer
-from .unified_language_server import UnifiedLanguageServer
-from .vscode_css_languageserver import VSCodeCSSLanguageServer
-from .vscode_html_languageserver import VSCodeHTMLLanguageServer
-from .vscode_json_languageserver import VSCodeJSONLanguageServer
-from .yaml_language_server import YAMLLanguageServer
 
-bash = BashLanguageServer()
-css = VSCodeCSSLanguageServer()
-dockerfile = DockerfileLanguageServerNodeJS()
-html = VSCodeHTMLLanguageServer()
-json = VSCodeJSONLanguageServer()
-md = UnifiedLanguageServer()
-py = KiteLanguageServer()
-ts = JavascriptTypescriptLanguageServer()
-yaml = YAMLLanguageServer()
-r = RLanguageServer()
+kite = KiteLanguageServer()
