d31e66faad10a8fe1eeed7329f9972b6e00d35c4
Edward Zhao <ed@kite.com>
2020-05-05 18:30:02 -0700

    Only trigger automatic completions on single character changes to the buffer

--- packages/jupyterlab-kite/src/adapters/codemirror/features/completion.ts
+++ packages/jupyterlab-kite/src/adapters/codemirror/features/completion.ts
@@ -23,6 +23,12 @@ export class Completion extends CodeMirrorLSPFeature {
   afterChange(change: CodeMirror.EditorChange): void {
     // TODO: maybe the completer could be kicked off in the handleChange() method directly; signature help still
     //  requires an up-to-date virtual document on the LSP side, so we need to wait for sync.
+    if (change.text.length && change.text[0].length > 1) {
+      // Use text[0].length => 1 as a proxy for a keystroke.
+      // Return early to prevent completions list from
+      // popping up right away after a completion is inserted.
+      return;
+    }
     let last_character = this.extract_last_character(change);
     if (this.completionCharacters.indexOf(last_character) > -1) {
       this.virtual_editor.console.log(
