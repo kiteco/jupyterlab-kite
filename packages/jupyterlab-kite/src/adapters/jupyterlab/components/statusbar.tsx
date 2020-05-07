4536f843978366a51154cfc3e62d48621be78f98
Edward Zhao <ed@kite.com>
2020-05-07 11:33:18 -0700

    Update status messages for Kite

--- packages/jupyterlab-kite/src/adapters/jupyterlab/components/statusbar.tsx
+++ packages/jupyterlab-kite/src/adapters/jupyterlab/components/statusbar.tsx
@@ -13,8 +13,8 @@ import { GroupItem, item, TextItem } from '@jupyterlab/statusbar';
 
 import { LabIcon } from '@jupyterlab/ui-components';
 import { JupyterLabWidgetAdapter } from '../jl_adapter';
-import { collect_documents, VirtualDocument } from '../../../virtual/document';
-import { LSPConnection } from '../../../connection';
+import { VirtualDocument } from '../../../virtual/document';
+import { IKiteStatus, LSPConnection } from '../../../connection';
 import { DocumentConnectionManager } from '../../../connection_manager';
 import { ILanguageServerManager } from '../../../tokens';
 
@@ -52,7 +52,6 @@ export class KiteStatus extends VDomRenderer<KiteStatus.Model> {
       <GroupItem spacing={4} title={this.model.long_message}>
         <this.model.status_icon.react top={'2px'} kind={'statusBar'} />
         <TextItem source={this.model.short_message} />
-        <TextItem source={this.model.feature_message} />
       </GroupItem>
     );
   }
@@ -67,22 +66,6 @@ export interface IStatus {
   detected_documents: Set<VirtualDocument>;
   status: StatusCode;
 }
-
-function collect_languages(virtual_document: VirtualDocument): Set<string> {
-  let documents = collect_documents(virtual_document);
-  return new Set(
-    [...documents].map(document => document.language.toLocaleLowerCase())
-  );
-}
-
-type StatusMap = Record<StatusCode, string>;
-
-const shortMessageByStatus: StatusMap = {
-  waiting: 'Waiting...',
-  initialized: 'Fully initialized',
-  initializing: 'Partially initialized',
-  connecting: 'Connecting...'
-};
 
 export namespace KiteStatus {
   /**
@@ -103,138 +86,13 @@ export namespace KiteStatus {
       this.icon.bindprops({ className: 'kite-logo' });
     }
 
-    get available_servers(): Array<SCHEMA.LanguageServerSession> {
-      return Array.from(this.language_server_manager.sessions.values());
-    }
-
-    get supported_languages(): Set<string> {
-      const languages = new Set<string>();
-      for (let server of this.available_servers) {
-        for (let language of server.spec.languages) {
-          languages.add(language.toLocaleLowerCase());
-        }
-      }
-      return languages;
-    }
-
-    private is_server_running(server: SCHEMA.LanguageServerSession): boolean {
-      for (let language of server.spec.languages) {
-        if (this.detected_languages.has(language.toLocaleLowerCase())) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    get documents_by_server(): Map<
-      SCHEMA.LanguageServerSession,
-      Map<string, VirtualDocument[]>
-    > {
-      let data = new Map();
-      if (!this.adapter?.virtual_editor) {
-        return data;
-      }
-
-      let main_document = this.adapter.virtual_editor.virtual_document;
-      let documents = collect_documents(main_document);
-      for (let document of documents.values()) {
-        let language = document.language.toLocaleLowerCase();
-        let servers = this.available_servers.filter(
-          server => server.spec.languages.indexOf(language) !== -1
-        );
-        if (servers.length > 1) {
-          console.warn('More than one server per language for' + language);
-        }
-        if (servers.length === 0) {
-          continue;
-        }
-        let server = servers[0];
-
-        if (!data.has(server)) {
-          data.set(server, new Map<string, VirtualDocument>());
-        }
-
-        let documents_map = data.get(server);
-
-        if (!documents_map.has(language)) {
-          documents_map.set(language, new Array<VirtualDocument>());
-        }
-
-        let documents = documents_map.get(language);
-        documents.push(document);
-      }
-      return data;
-    }
-
-    get servers_available_not_in_use(): Array<SCHEMA.LanguageServerSession> {
-      return this.available_servers.filter(
-        server => !this.is_server_running(server)
+    get status(): IKiteStatus {
+      const activeDocument = this.adapter.virtual_editor.virtual_document;
+      const connection = this.connection_manager.connections.get(
+        activeDocument.id_path
       );
-    }
-
-    get detected_languages(): Set<string> {
-      if (!this.adapter?.virtual_editor) {
-        return new Set<string>();
-      }
-
-      let document = this.adapter.virtual_editor.virtual_document;
-      return collect_languages(document);
-    }
-
-    get missing_languages(): Array<string> {
-      // TODO: false negative for r vs R?
-      return [...this.detected_languages].filter(
-        language => !this.supported_languages.has(language.toLocaleLowerCase())
-      );
-    }
-
-    get status(): IStatus {
-      const detected_documents = this._connection_manager.documents;
-      let connected_documents = new Set<VirtualDocument>();
-      let initialized_documents = new Set<VirtualDocument>();
-
-      detected_documents.forEach((document, id_path) => {
-        let connection = this._connection_manager.connections.get(id_path);
-        if (!connection) {
-          return;
-        }
-
-        if (connection.isConnected) {
-          connected_documents.add(document);
-        }
-        if (connection.isInitialized) {
-          initialized_documents.add(document);
-        }
-      });
-
-      // there may be more open connections than documents if a document was recently closed
-      // and the grace period has not passed yet
-      let open_connections = new Array<LSPConnection>();
-      this._connection_manager.connections.forEach((connection, path) => {
-        if (connection.isConnected) {
-          open_connections.push(connection);
-        }
-      });
-
-      let status: StatusCode;
-      if (detected_documents.size === 0) {
-        status = 'waiting';
-        // TODO: instead of detected documents, I should use "detected_documents_with_LSP_servers_available"
-      } else if (initialized_documents.size === detected_documents.size) {
-        status = 'initialized';
-      } else if (connected_documents.size === detected_documents.size) {
-        status = 'initializing';
-      } else {
-        status = 'connecting';
-      }
-
-      return {
-        open_connections,
-        connected_documents,
-        initialized_documents,
-        detected_documents: new Set([...detected_documents.values()]),
-        status
-      };
+      console.log('Found status:', connection.kiteStatus);
+      return connection.kiteStatus;
     }
 
     get status_icon(): LabIcon {
@@ -242,55 +100,17 @@ export namespace KiteStatus {
     }
 
     get short_message(): string {
-      if (!this.adapter) {
-        return 'not initialized';
+      if (!this.adapter || !this.status.status) {
+        return 'Kite: not running';
       }
-      return shortMessageByStatus[this.status.status];
-    }
-
-    get feature_message(): string {
-      return this.adapter?.status_message?.message || '';
+      return 'Kite: ' + this.status.short;
     }
 
     get long_message(): string {
-      if (!this.adapter) {
-        return 'not initialized';
+      if (!this.adapter || !this.status.status) {
+        return 'Kite is not reachable.';
       }
-      let status = this.status;
-      let msg = '';
-      const plural = status.detected_documents.size > 1 ? 's' : '';
-      if (status.status === 'waiting') {
-        msg = 'Waiting for documents initialization...';
-      } else if (status.status === 'initialized') {
-        msg = `Fully connected & initialized (${status.detected_documents.size} virtual document${plural})`;
-      } else if (status.status === 'initializing') {
-        const uninitialized = new Set<VirtualDocument>(
-          status.detected_documents
-        );
-        for (let initialized of status.initialized_documents.values()) {
-          uninitialized.delete(initialized);
-        }
-        // servers for n documents did not respond ot the initialization request
-        msg = `Fully connected, but ${uninitialized.size}/${
-          status.detected_documents.size
-        } virtual document${plural} stuck uninitialized: ${[...uninitialized]
-          .map(document => document.id_path)
-          .join(', ')}`;
-      } else {
-        const unconnected = new Set<VirtualDocument>(status.detected_documents);
-        for (let connected of status.connected_documents.values()) {
-          unconnected.delete(connected);
-        }
-
-        msg = `${status.connected_documents.size}/${
-          status.detected_documents.size
-        } virtual document${plural} connected (${
-          status.open_connections.length
-        } connections; waiting for: ${[...unconnected]
-          .map(document => document.id_path)
-          .join(', ')})`;
-      }
-      return msg;
+      return this.status.long;
     }
 
     get adapter(): JupyterLabWidgetAdapter | null {
