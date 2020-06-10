51508bea3b8a56527e57c0497842ddf12bbec994
Edward Zhao <ed@kite.com>
2020-06-10 15:59:10 -0700

    Refactor kite_commands

--- packages/jupyterlab-kite/src/kite_commands.ts
+++ packages/jupyterlab-kite/src/kite_commands.ts
@@ -3,51 +3,63 @@ import { ICommandPalette } from '@jupyterlab/apputils';
 import { CommandRegistry } from '@lumino/commands';
 
 const category = 'kite';
-const commandMap = new Map<string, CommandRegistry.ICommandOptions>([
-  [
-    'tutorial',
-    {
+interface IKiteCommand {
+  id: string;
+  options: CommandRegistry.ICommandOptions;
+}
+const cmdIds = {
+  tutorial: 'kite:tutorial',
+  copilot: 'kite:copilot',
+  settings: 'kite:settings',
+  help: 'kite:help'
+};
+
+const paletteCommands: ReadonlyArray<IKiteCommand> = [
+  {
+    id: cmdIds.tutorial,
+    options: {
       label: 'Kite: Tutorial',
       execute: () => {
         console.log('Kite: Tutorial');
       }
     }
-  ],
-  [
-    'copilot',
-    {
+  },
+  {
+    id: cmdIds.copilot,
+    options: {
       label: 'Kite: Open Copilot',
       execute: () => {
         window.open('kite://home');
       }
     }
-  ],
-  [
-    'settings',
-    {
+  },
+  {
+    id: cmdIds.settings,
+    options: {
       label: 'Kite: Engine Settings',
       execute: () => {
         window.open('kite://settings');
       }
     }
-  ],
-  [
-    'help',
-    {
+  },
+  {
+    id: cmdIds.help,
+    options: {
       label: 'Kite: Help',
       execute: () => {
         window.open('https://help.kite.com/');
       }
     }
-  ]
-]);
+  }
+];
 
 export function registerKiteCommands(
   app: JupyterFrontEnd,
   palette: ICommandPalette
 ) {
-  commandMap.forEach((options, id) => {
-    app.commands.addCommand(id, options);
-    palette.addItem({ command: id, category });
+  // Register palette commands
+  paletteCommands.forEach(cmd => {
+    app.commands.addCommand(cmd.id, cmd.options);
+    palette.addItem({ command: cmd.id, category });
   });
 }
