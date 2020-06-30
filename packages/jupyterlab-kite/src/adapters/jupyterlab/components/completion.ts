c1619063c28139d132f65d8d3eb16f4d562bce89
Ryan Berg <ryan@kite.com>
2020-06-30 14:19:55 -0700

    only fetch kernel completions after certain trigger characters

--- packages/jupyterlab-kite/src/adapters/jupyterlab/components/completion.ts
+++ packages/jupyterlab-kite/src/adapters/jupyterlab/components/completion.ts
@@ -165,6 +165,7 @@ export class KiteConnector extends DataConnector<
       };
 
       const isManual = this._trigger_kind === CompletionTriggerKind.Invoked;
+
       /**
        * """
        * """
@@ -172,14 +173,25 @@ export class KiteConnector extends DataConnector<
        */
       const should_suppress_strings = !isManual && token.type === 'string';
 
+      /**
+       * Kernel completions should only be fetched after an alphanumeric character or . ' "
+       */
+      const kernelTriggerRegex = /^[0-9a-zA-Z\.\'\"]+/;
+
       const kernelPromise = () => {
         /**
          * Don't fetch kernel completions if:
          * - No kernel connector
          * - No request object
          * - Token type is string (otherwise kernel completions appear within docstrings)
+         * - Preceding character isn't a kernel trigger character
          */
-        if (!this._kernel_connector || !request || should_suppress_strings) {
+        if (
+          !this._kernel_connector ||
+          !request ||
+          should_suppress_strings ||
+          !kernelTriggerRegex.test(typed_character)
+        ) {
           return KiteConnector.EmptyIReply;
         }
         return this._kernel_connector.fetch(request).catch(() => {
