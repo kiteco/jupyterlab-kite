043c1a007f4c461e280caf57488e8766d2a9b0e2
naman <1977419+metalogical@users.noreply.github.com>
2020-07-07 18:24:10 -0700

    style
    [skip ci]

--- packages/jupyterlab-kite/src/adapters/jupyterlab/kite_model.ts
+++ packages/jupyterlab-kite/src/adapters/jupyterlab/kite_model.ts
@@ -7,60 +7,55 @@ import {
 import { Text } from '@jupyterlab/coreutils';
 
 export class KiteModel extends CompleterModel {
-         private _state: Completer.ITextState | undefined;
+  private _state: Completer.ITextState | undefined;
 
-         constructor() {
-           super();
-         }
+  constructor() {
+    super();
+  }
 
-         get state(): Completer.ITextState | undefined {
-           return this._state;
-         }
+  get state(): Completer.ITextState | undefined {
+    return this._state;
+  }
 
-         set state(newState: Completer.ITextState | undefined) {
-           this._state = newState;
-         }
+  set state(newState: Completer.ITextState | undefined) {
+    this._state = newState;
+  }
 
-         handleCursorChange(change: Completer.ITextState) {
-           super.handleCursorChange(change);
-           const prevState = this.state;
-           if (prevState) {
-             /**
-              * Reset model unless cursor change is result of typing a new character.
-              */
-             if (
-               change.column - prevState.column === 1 &&
-       
-               change.line - prevState.line === 0
-             ) {
-               return;
-             }
-             this.reset(true);
-           }
-         }
+  handleCursorChange(change: Completer.ITextState) {
+    super.handleCursorChange(change);
+    const prevState = this.state;
+    if (prevState) {
+      if (change.column - prevState.column === 1 && change.line - prevState.line === 0) {
+        // single char insertion
+        return;
+      }
+      if (change.column === 1 && change.line - prevState.line === 1) {
+        // newline insertion
+        return
+      }
+      // otherwise reset the model
+      this.reset(true);
+    }
+  }
 
-         handleTextChange(change: Completer.ITextState) {
-           super.handleTextChange(change);
-           this.state = change;
-         }
+  handleTextChange(change: Completer.ITextState) {
+    super.handleTextChange(change);
+    this.state = change;
+  }
 
-         /**
-          * Re-implementation of private method _updateModel.
-          * https://github.com/jupyterlab/jupyterlab/blob/1df0e18951194bb5ec230e76441e8108e0b472e7/packages/completer/src/handler.ts#L421
-          * Enables completer to fully update model state when completions are force updated in JupyterLabWidgetAdapter.
-          */
-         update(
-           reply: CompletionHandler.ICompletionItemsReply,
-           query: string,
-           state: Completer.ITextState
-         ) {
-           this.original = state;
-           this.query = query;
-           const text = state.text;
-           // Update the cursor.
-           this.cursor = {
-             start: Text.charIndexToJsIndex(reply.start, text),
-             end: Text.charIndexToJsIndex(reply.end, text)
-           };
-         }
-       }
+  /**
+   * Re-implementation of private method _updateModel.
+   * https://github.com/jupyterlab/jupyterlab/blob/1df0e18951194bb5ec230e76441e8108e0b472e7/packages/completer/src/handler.ts#L421
+   * Enables completer to fully update model state when completions are force updated in JupyterLabWidgetAdapter.
+   */
+  update(reply: CompletionHandler.ICompletionItemsReply, query: string, state: Completer.ITextState) {
+    this.original = state;
+    this.query = query;
+    const text = state.text;
+    // Update the cursor.
+    this.cursor = {
+      start: Text.charIndexToJsIndex(reply.start, text),
+      end: Text.charIndexToJsIndex(reply.end, text)
+    };
+  }
+}
