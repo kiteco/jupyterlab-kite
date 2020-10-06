a6b6ccfe25a97156edfaf090d61452c9758cb74f
Ryan Berg <ryan@kite.com>
2020-10-06 14:31:10 -0700

    fix typo

--- packages/jupyterlab-kite/src/adapters/jupyterlab/components/status_model.ts
+++ packages/jupyterlab-kite/src/adapters/jupyterlab/components/status_model.ts
@@ -115,7 +115,7 @@ export class KiteStatusModel extends VDomModel {
       return {
         text: 'Kite: cannot reach jupyter-kite',
         tooltip:
-          'jupyter-kite serverextension could not be reached, and might not be installed.'
+          'jupyter-kite server extension could not be reached, and might not be installed.'
       };
     }
 
